0a326df438fe79cb2cd9971a3d77ae383870b5b0
传奇坐牢王 <2641210631@qq.com>
2026-04-20 14:47:00 +0800

    chore: update frontend/src/App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,9 @@
-function App() {
-  return <div>AI Voice Chat</div>;
-}
+import VoiceChat from './components/VoiceChat'
 
-export default App;
+export default function App() {
+  return (
+    <div className="app">
+      <VoiceChat />
+    </div>
+  )
+}
